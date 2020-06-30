d31e9d7cedf29a96c39a8ddf3dff7ab84f732465
jonathancsr <jonathancsrr@gmail.com>
2020-06-30 20:50:25 -0300

    add: Global Style

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import React,{useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-import api from './Services/Api'
+import GlobalStyle from "./styles/global";
+
+import Initial from './pages/Initial'
 
 function App() {
-  const [countShips, setCountShips] = useState(null)
-  useEffect( () => {
-    api.get('/starships/').then(data => {
-      console.log(data)
-      setCountShips(data.data)
-    });
-  },[]);
-  async function handleApiRequest(){
-    }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {countShips && countShips.count}
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <>
+      <Initial/>
+      <GlobalStyle/>
+    </>
   );
-}
+};
 
 export default App;
