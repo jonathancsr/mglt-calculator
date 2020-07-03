c61d91928640c8560f3b35859e8a759435cdf628
jonathancsr <jonathancsrr@gmail.com>
2020-07-02 22:00:44 -0300

    add: new initial page

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,57 +1,63 @@
-import React,{useCallback, useEffect, useRef, useContext} from 'react'
+import React, { useCallback, useEffect, useRef, useContext } from 'react';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { IoMdRocket,IoMdSearch } from "react-icons/io";
-import {useHistory} from 'react-router-dom'
+import { IoMdRocket, IoMdSearch } from 'react-icons/io';
+import { useHistory } from 'react-router-dom';
 
 import logo from '../../assets/mgtl-logo.svg';
-import {StarshipsContext} from '../../context/StarshipsContext' 
-import getValidationErrors from '../../utils/getValidationErrors'
-import Input from '../../components/Input'
+import { StarshipsContext } from '../../context/StarshipsContext';
+import getValidationErrors from '../../utils/getValidationErrors';
+import Input from '../../components/Input';
 
-import {Container} from './styles'
+import { Container, Header } from './styles';
 
-
-function Initial(){
+function Initial() {
   const formRef = useRef(null);
-  
+
   const history = useHistory();
-  const {getStarships, getSpaceshipsStopsInOrder} = useContext(StarshipsContext);
+  const { getStarships, getSpaceshipsStopsInOrder } = useContext(
+    StarshipsContext,
+  );
 
-  useEffect( () =>{
+  useEffect(() => {
     getStarships();
-  },[getStarships])
+  }, [getStarships]);
 
-  const handleSubmit = useCallback(async(data)=>{
-    formRef.current.setErrors({});
-    try {
-      const schema = Yup.object().shape({
-        distance: Yup.string()
-          .required('Distance to be traveled mandatory'),
-      });
+  const handleSubmit = useCallback(
+    async data => {
+      formRef.current.setErrors({});
+      try {
+        const schema = Yup.object().shape({
+          distance: Yup.string().required('Distance to be traveled mandatory'),
+        });
 
-     await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      getSpaceshipsStopsInOrder(data.distance);
-      history.push('/dashboard')
-    } catch (error) {
-      const errors = getValidationErrors(error);
-      formRef.current.setErrors(errors);
-    }
-  }, [history,getSpaceshipsStopsInOrder])
+        getSpaceshipsStopsInOrder(data.distance);
+        history.push('/dashboard');
+      } catch (error) {
+        const errors = getValidationErrors(error);
+        formRef.current.setErrors(errors);
+      }
+    },
+    [history, getSpaceshipsStopsInOrder],
+  );
 
   return (
     <Container>
-      <img src={logo} alt="logo" />
-      <h2>Input distance to receive a list with the best starships to fly</h2>
+      <Header>
+        <img src={logo} alt="logo" />
+        <p>If you need to travel, we'll tell you what starship to pick</p>
+      </Header>
       <Form ref={formRef} onSubmit={handleSubmit}>
+        <p>What's the distance you need to travel?</p>
         <Input
           name="distance"
           icon={IoMdSearch}
           type="number"
-          placeholder="1.000.000 mgtl's"
+          placeholder="e.g: 1.000.000 mgtl's"
         />
         <button type="submit">
           <IoMdRocket size={30} />
--- src/pages/Initial/styles.jsx
+++ src/pages/Initial/styles.jsx
@@ -7,38 +7,59 @@ export const Container = styled.div`
   flex-direction: column;
   place-content: center;
   align-items: center;
-  
-  img{
-    margin-bottom:10px;
+  font-family: 'Roboto', sans-serif;
+
+  p {
+    width: 500px;
   }
 
-  h2{
-    font-family: 'Roboto', sans-serif;
-    font-weight:300;
+  h2 {
+    font-weight: 300;
     font-size: 20px;
   }
-  
-  form{
+
+  form {
     display: flex;
     align-items: center;
     flex-direction: column;
-    margin-top:24px;
-    
-    button{
-      width:50px;
-      height:50px;
-      margin-top:24px;
-      background:#F9AC34;
-      border-radius:50%;
-      border:0;
+    margin-top: 24px;
+
+    button {
+      width: 50px;
+      height: 50px;
+      margin-top: 24px;
+      background: #f9ac34;
+      border-radius: 50%;
+      border: 0;
       transition: transform 0.2s;
 
-      &:hover{
-        -webkit-box-shadow: 0px 2px 15px 2px rgba(54,134,195,0.7);
-        -moz-box-shadow:    0px 2px 15px 2px rgba(54,134,195,0.7);
-        box-shadow:         0px 2px 15px 2px rgba(255,255,255,0.15);
-        transform: scale(1.2) ;
+      &:hover {
+        -webkit-box-shadow: 0px 2px 15px 2px rgba(54, 134, 195, 0.7);
+        -moz-box-shadow: 0px 2px 15px 2px rgba(54, 134, 195, 0.7);
+        box-shadow: 0px 2px 15px 2px rgba(255, 255, 255, 0.15);
+        transform: scale(1.2);
       }
     }
+    p {
+      font-size: 14px;
+      color: #f9ac34;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  align-items: center;
+
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  text-align: center;
+
+  margin-bottom: 40px;
+  img {
+    margin-bottom: 16px;
   }
 `;
